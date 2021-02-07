76171b922973e5bb668b51efa953524ef61dec0c
yorojanine <j9yoro@gmail.com>
2021-02-07 00:34:06 -0800

    module

---
+++ static/js/app.js
@@ -0,0 +1,77 @@
+// import the data from data.js
+const tableData = data;
+
+//reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+//simple JavaScript console.log statement
+function printHello() {
+    console.log("Hello there!");
+}
+
+printHello = () => "Hello there!";
+
+// Original addition function
+function addition(a, b) {
+    return a + b;
+  }
+
+// Converted to an arrow function
+addition = (a, b) => a + b;
+
+// Original doubleAddition function
+function doubleAddition(c, d) {
+    var total = addition(c, d) * 2;
+    return total;
+  }
+
+doubleAddition = (c, d) => addition(c, d) * 2;
+
+let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+
+function listLoop(userList) {
+    for (var i = 0; i < userList.length; i++) {
+      console.log(userList[i]);
+    }
+ }
+
+ listLoop(friends);
+
+ let vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+
+ for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love" + vegetables[i]) ;
+    }
+
+function buildTable(data) {
+    tbody.html("");
+}   
+
+data.forEach((dataRow) => {
+    let row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+    let cell = row.append("td");
+    cell.text(val);
+    }
+    );
+});
+
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
---
+++ static/js/class.js
@@ -0,0 +1,5 @@
+let movieScore = [4.4, 3.3, 5.9, 8.8, 1.2, 5.2, 7.4, 7.5, 7.2, 9.7, 4.2, 6.9];
+
+function average(movieScore){
+    return movieScore.reduce((a,b) => (a+b))/movieScore.length
+}
